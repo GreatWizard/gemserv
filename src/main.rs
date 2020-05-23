5c00fb24fde023f64fbfb2fca4d4429eac1b9983
int 80h <int@80h.dev>
2020-05-23 17:55:26 -0400

    Fixed bugs in cgi-everywhere

--- src/main.rs
+++ src/main.rs
@@ -244,10 +244,6 @@ async fn handle_connection(
                     con.send_status(Status::CGIError, None).await?;
                     return Ok(());
                 }
-            } else {
-                logger::logger(con.peer_addr, Status::CGIError, &request);
-                con.send_status(Status::CGIError, None).await?;
-                return Ok(());
             }
             },
             None => {
@@ -259,6 +255,12 @@ async fn handle_connection(
         }
     }
 
+    if perm.mode() & 0o0111 == 0o0111  {
+        logger::logger(con.peer_addr, Status::NotFound, &request);
+        con.send_status(Status::NotFound, None).await?;
+        return Ok(());
+    }
+    
     if perm.mode() & 0o0444 != 0o0444  {
         logger::logger(con.peer_addr, Status::NotFound, &request);
         con.send_status(Status::NotFound, None).await?;
